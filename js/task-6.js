5ceb600afafdffca782a449f344656c912cb7eef
Nikita-Maiorko <nmaiorko@gmail.com>
2025-03-09 22:27:39 +0200

    initial files

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,43 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const controls = document.querySelector('#controls');
+  const input = controls.querySelector('input');
+  const createBtn = controls.querySelector('[data-create]');
+  const destroyBtn = controls.querySelector('[data-destroy]');
+  const boxes = document.querySelector('#boxes');
+
+  function createBoxes(amount) {
+    const boxElements = [];
+    let size = 30;
+
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      boxElements.push(box);
+      size += 10;
+    }
+
+    boxes.innerHTML = '';
+    boxes.append(...boxElements);
+  }
+
+  function destroyBoxes() {
+    boxes.innerHTML = '';
+  }
+
+  createBtn.addEventListener('click', () => {
+    const amount = Number(input.value);
+
+    if (amount >= 1 && amount <= 100) {
+      createBoxes(amount);
+      input.value = '';
+    }
+  });
+
+  destroyBtn.addEventListener('click', destroyBoxes);
+});
